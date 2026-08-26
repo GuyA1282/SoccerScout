8a3e6c321763f9129b03d53bd91bcd7e77eaf4f9
Guy <guy12820@gmail.com>
2026-08-26 16:30:27 +0300

    .

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
