3b2f49842525a7387e0ac3644923bb603c7ddf3f
Guy <guy12820@gmail.com>
2026-08-26 15:52:39 +0300

    Added mock player list for testing, before deciding what player DB i'll use.

---
+++ extract.py
@@ -0,0 +1,9 @@
+def fetch_from_source():
+	mock_players = [
+		{"id": 1, "name": "Eran Zahavi", "nationality": "Israel", "second_nationality": "", "birth_city": "Rishon LeZion"},
+		{"id": 2, "name": "David Cohen", "nationality": "Argentina", "second_nationality": "Italy",
+		 "birth_city": "Buenos Aires"},
+		{"id": 3, "name": "James Rodriguez", "nationality": "Colombia", "second_nationality": "", "birth_city": "Cucuta"},
+		{"id": 4, "name": "Moshe Miller", "nationality": "USA", "second_nationality": "", "birth_city": "Chicago"}
+	]
+	return mock_players
